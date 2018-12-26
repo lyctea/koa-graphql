836a842e6e456c3f190463bb5c046d3cd85a059f
lyctea <396136337@qq.com>
2018-12-26 10:29:50 +0800

    feat: 数据模型，接口联调

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ import KoaStatic from 'koa-static';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 
+import { database } from './mongodb'; // 引入mongodb
+import { saveInfo, fetchInfo } from './controllers/info'; // 引入info controller
+import { saveStudent, fetchStudent, fetchStudentDetail } from './controllers/student'; // 引入 student controller
+
+database(); // 链接数据库并且初始化数据模型
+
 const app = new Koa();
 const router = new Router();
 
-// 使用中间件
 app.use(bodyParser());
 app.use(KoaStatic(__dirname + '/public'));
 
-// 设置路由
 router.get('/test', (ctx, next) => {
-  ctx.body = 'test body';
+  ctx.body = 'test page';
 });
 
-app.use(router.routes())
+// 设置每一个路由对应的相对的控制器
+router.post('/saveinfo', saveInfo);
+router.get('/info', fetchInfo);
+
+router.post('/savestudent', saveStudent);
+router.get('/student', fetchStudent);
+router.get('/studentDetail', fetchStudentDetail);
+
+app
+  .use(router.routes())
   .use(router.allowedMethods());
 
 app.listen(4000);
 
-console.log('graphQL server listen prot: ' + 4000);
+console.log('graphQL server listen port: ' + 4000);
